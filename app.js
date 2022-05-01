228f75713bde1d4a2cb634857d1bc34cf07531d2
Anthony11-hub <mollycruz966@gmail.com>
2022-04-30 19:27:54 -0700

    modifying app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 function loadTexture(path) {
   return new Promise((resolve) => {
-    const img = new Image()
+    const img = new Image()//creating an image object to load and draw an image asset
     img.src = path
     img.onload = () => {
+      // image loaded and ready to be used
       resolve(img)
     }
   })
@@ -10,15 +11,29 @@ function loadTexture(path) {
 
 function createEnemies(ctx, canvas, enemyImg) {
   // TODO draw enemies
+  const MONSTER_TOTAL = 5;
+  const MONSTER_WIDTH = MONSTER_TOTAL * 98;
+  const START_X = (canvas.width - MONSTER_WIDTH)/2;
+  const STOP_X = START_X + MONSTER_WIDTH;
+
+  for(let x = START_X; x<STOP_X; x+=98){
+    for (let y = 0;y<50*5;y+=50){
+      ctx.drawImage(enemyImg, x, y);
+    }
+  }
 }
 
-window.onload = async () => {
+window.onload = async () => {//use of fat arrow functions
   canvas = document.getElementById('canvas')
   ctx = canvas.getContext('2d')
   // TODO load textures
-
+  const enemyImg = await loadTexture('assets/enemyShip.png')
+  const heroImg = await loadTexture('assets/player.png');
   // TODO draw black background
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0,0, canvas.width, canvas.height);
   // TODO draw hero
+  ctx.drawImage(heroImg, canvas.width/2 - 45,canvas.height - canvas.height/4)
   // TODO uncomment the next line when you add enemies to screen
-  //createEnemies(ctx, canvas, enemyImg);
+  createEnemies(ctx, canvas, enemyImg);
 }
